c78a97c99586a0462aa0efd3f508ea5f98eee685
Kimmingyu <d2rkjc38@gmail.com>
2025-11-04 20:35:28 +0900

    Drill#10 제출 - 2024180010 김민규

--- main.py
+++ main.py
@@ -6,4 +6,7 @@ import play_mode as start_mode
 open_canvas(1600, 600)
 game_framework.run(start_mode)
 close_canvas()
-
+#1픽셀 : 3cm
+#새의 크기 : (75,75), 약 180cm x 145cm
+#새 이동 속도 : 시속 40km
+#새의 날개짓 속도 : 초당 3번
